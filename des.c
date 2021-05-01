60755769c800e929e847e09656e97439b1eff71f
ako800 <81560559+ako800@users.noreply.github.com>
2021-05-01 11:30:35 +0200

    Update des.c

--- des.c
+++ des.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main (){
-    printf("Algoritmo DES");
+	int i;
+	for (i = 0; i < 5; i++) {
+		printf("Algoritmo DES");
+		printf("\n");
+	}
+	printf("Fin del ciclo");
+	
+	return 0;
 }
